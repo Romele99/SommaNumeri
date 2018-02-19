4f579f1a30c8db974640527cebe7d3c26aba59bd
Giuseppe Lazzarini <beppolazza@gmail.com>
2018-02-19 12:39:04 +0100

    aggiunto somma 200

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -26,6 +26,14 @@ public class Sommatore
 		return x;
 			
 	}
+	
+	public static int somma200(int x)
+	{
+		for (int i = 0; i < 200; i++)
+			x++;
+		return x;
+			
+	}
 
 	public static void main(String[] args) 
 	{
@@ -35,6 +43,7 @@ public class Sommatore
 		System.out.println(somma10(x));
 		System.out.println(somma20(x));
 		System.out.println(somma100(x));
+		System.out.println(somma200(x));
 
 	}
 
